1bfa6fcacd0ff40c88b2036b9431896b006ebea5
praneeth9666 <43279110+praneeth9666@users.noreply.github.com>
2019-10-27 23:53:58 +0530

    Update Q6_IMT2016109.s

--- Q6_IMT2016109.s
+++ Q6_IMT2016109.s
@@ -4,16 +4,16 @@
     EXPORT __main 
     ENTRY 
 __main    FUNCTION 
-    	MOV R0,#4		;Initializing the registers with three numbers to compar
+    	MOV R0,#4		;Initializing the registers with two numbers to compute the GCD
 	MOV R1,#1	
-	MOV R5,#0x20000000	;Initializing the register with the address where the largest number is stored
+	MOV R5,#0x20000000	;Initializing the register with the address 
 loop1	CMP R0,R1		;COmparing a,b
 	BEQ stop		;If a=b the stop loop
 	ITE gt			;If a>b the SUBGT else SUBLE
 	SUBGT R0,R0,R1
 	SUBLE R1,R1,R0
 	B loop1			;branch to loop1
-stop    STR R0,[R5,#0]		;Storing 	
-		B stop 
+stop    STR R0,[R5,#0]		;Storingthe HCF at the given register
+	B stop 
         ENDFUNC 
         END
